268a5ab1539fe523859f55781f0b95487e4a36f5
yuc <chuan.yu@autodesk.com>
2020-02-27 13:44:09 +0800

    Adjust new list view to do model validation

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -4,7 +4,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 
-MAX_WAIT = 3
+MAX_WAIT = 5
 
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -11,12 +11,12 @@ class ItemValidationTest(FunctionalTest):
 
         self.wait_for(lambda: self.assertEqual(
             self.browser.find_element_by_css_selector('.has-error').text,
-            "You can't have an empy list item"
+            "You can't have an empty list item"
         ))
 
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1. Buy milk')
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
